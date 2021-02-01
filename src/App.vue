446aa3acbdcbff2e6c0a9bd03af99a8a83f82709
Andreas Nedbal <andy@pixelde.su>
2021-02-01 23:12:16 +0100

    :sparkles: add main App component

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <h1><i>omochikaerii~</i> - Higurashi Marathon Tracker</h1>
+    <p class="text-muted">
+      This webpage is a public tracker of <a href="https://twitter.com/pixeldesu">@pixeldesu</a>'s journey through all of <b>Higurashi no Naku Koro ni</b> by 07th Expansion.
+    </p>
+    <p>
+      Currently {{ completedEntryCount}} out of the {{ entryCount }} total entries have been finished, amounting to about {{ totalCompletionPercentage}}% completion!
+    </p>
+    <div class="progress mb-5">
+      <div class="progress-bar bg-success" role="progressbar" :style="'width: ' + totalCompletionPercentage + '%'" :aria-valuenow="totalCompletionPercentage" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+
+    <marathon-section v-for="seriesEntry in seriesData" :key="seriesEntry.title" :data="seriesEntry"/>
+    <footer>
+      <hr/>
+      <p class="text-muted">
+        <a class="text-reset font-monospace text-decoration-none" href="https://github.com/pixeldesu/omochikaerii">omochikaerii~</a> built by <a class="text-reset" href="https://github.com/pixeldesu">pixeldesu</a>
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import MarathonSection from './components/MarathonSection.vue'
+import seriesData from './data'
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      seriesData: seriesData
+    }
+  },
+  components: {
+    MarathonSection
+  },
+  computed: {
+    entryCount: function() {
+      let allEntries = []
+
+      this.seriesData.forEach((seriesEntry) => {
+        allEntries = allEntries.concat(seriesEntry.entries)
+      })
+
+      return allEntries.length
+    },
+    completedEntryCount: function() {
+      let allEntries = []
+
+      this.seriesData.forEach((seriesEntry) => {
+        allEntries = allEntries.concat(seriesEntry.entries)
+      })
+
+      return (allEntries.filter((entry) => entry.done === true)).length
+    },
+    totalCompletionPercentage: function() {
+      let allEntries = []
+
+      this.seriesData.forEach((seriesEntry) => {
+        allEntries = allEntries.concat(seriesEntry.entries)
+      })
+
+      const completedEntries = (
+        allEntries.filter((entry) => entry.done === true)
+        ).length
+
+      console.log(allEntries.length)
+
+      return ((completedEntries / allEntries.length) * 100).toFixed()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
