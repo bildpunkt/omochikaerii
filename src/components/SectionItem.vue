69dca0997e6e7e04c88f7d95689d7e8d12e0c222
Andreas Nedbal <andy@pixelde.su>
2021-02-01 23:12:50 +0100

    :sparkles: add section item component

---
+++ src/components/SectionItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="classes">
+    {{ entry.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionItem',
+  props: {
+    entry: Object
+  },
+  computed: {
+    classes: function() {
+      return {
+        'list-group-item': true,
+        'list-group-item-success': this.entry.done
+      }
+    }
+  }
+}
+</script>
