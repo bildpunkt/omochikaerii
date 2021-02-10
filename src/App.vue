f16a5999cd8814fa429df232caa940b25f625c13
Andreas Nedbal <andy@pixelde.su>
2021-02-10 22:37:04 +0100

    :recycle: use Reactivity API to clean up data values

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
       This webpage is a public tracker of <a href="https://twitter.com/pixeldesu">@pixeldesu</a>'s journey through all of <b>Higurashi no Naku Koro ni</b> by 07th Expansion.
     </p>
     <p>
-      Currently {{ completedEntryCount }} (weight: <code v-html="completedWeight" />) out of the {{ entryCount }} total entries (weight: <code v-html="entryWeight" />) have been finished, amounting to about {{ totalCompletionPercentage}}% completion! The list is weighted by the approximate time a single entry
+      Currently {{ global.completedEntryCount }} (weight: <code v-html="global.completedEntryWeight" />) out of the {{ global.entryCount }} total entries (weight: <code v-html="global.entryWeight" />) have been finished, amounting to about {{ global.completedPercentage }}% completion! The list is weighted by the approximate time a single entry
       takes to be finished (in hours), that's why the completion percentage does probably not match up with the actual finished count.
     </p>
     <div class="progress mb-5">
-      <div class="progress-bar bg-success" role="progressbar" :style="'width: ' + totalCompletionPercentage + '%'" :aria-valuenow="totalCompletionPercentage" aria-valuemin="0" aria-valuemax="100"></div>
+      <div class="progress-bar bg-success" role="progressbar" :style="'width: ' + global.completedPercentage + '%'" :aria-valuenow="global.completedPercentage" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <marathon-section v-for="seriesEntry in seriesData" :key="seriesEntry.title" :data="seriesEntry"/>
@@ -24,93 +24,23 @@
 
 <script>
 import MarathonSection from './components/MarathonSection.vue'
+import global from './state'
 import seriesData from './data'
 
 export default {
   name: 'App',
   data: function() {
     return {
-      seriesData: seriesData
+      seriesData: seriesData,
+      global
     }
   },
   components: {
     MarathonSection
   },
-  computed: {
-    entryCount: function() {
-      let allEntries = []
-
-      this.seriesData.forEach((seriesEntry) => {
-        allEntries = allEntries.concat(seriesEntry.entries)
-      })
-
-      return allEntries.length
-    },
-    entryWeight: function() {
-      let allEntries = []
-      let fullWeight = 0
-
-      this.seriesData.forEach((seriesEntry) => {
-        allEntries = allEntries.concat(seriesEntry.entries)
-      })
-
-      allEntries.forEach((entry) => {
-        fullWeight += entry.weight
-      })
-
-      return fullWeight.toFixed(2)
-    },
-    completedEntryCount: function() {
-      let allEntries = []
-
-      this.seriesData.forEach((seriesEntry) => {
-        allEntries = allEntries.concat(seriesEntry.entries)
-      })
-
-      return (allEntries.filter((entry) => entry.done === true)).length
-    },
-    completedWeight: function() {
-      let allEntries = []
-      let completedWeight = 0
-
-      this.seriesData.forEach((seriesEntry) => {
-        allEntries = allEntries.concat(seriesEntry.entries)
-      })
-
-      const completedEntries = (
-        allEntries.filter((entry) => entry.done === true)
-        )
-
-      completedEntries.forEach((entry) => {
-        completedWeight += entry.weight
-      })
-
-      return completedWeight.toFixed(2)
-    },
-    totalCompletionPercentage: function() {
-      let allEntries = []
-      let fullWeight = 0
-      let completedWeight = 0
-
-      this.seriesData.forEach((seriesEntry) => {
-        allEntries = allEntries.concat(seriesEntry.entries)
-      })
-
-      allEntries.forEach((entry) => {
-        fullWeight += entry.weight
-      })
-
-      const completedEntries = (
-        allEntries.filter((entry) => entry.done === true)
-        )
-
-      completedEntries.forEach((entry) => {
-        completedWeight += entry.weight
-      })
-
-      return ((completedWeight / fullWeight) * 100).toFixed()
-    }
-  }
+  provide: {
+    global
+  },
 }
 </script>
 
---
+++ src/state.js
@@ -0,0 +1,26 @@
+import { reactive, computed } from 'vue'
+import data from './data'
+
+const state = reactive({
+  seriesData: data
+})
+
+const entries = computed(() => state.seriesData.flatMap((series) => series.entries))
+const entryCount = computed(() => entries.value.length)
+const entryWeight = computed(() => entries.value.reduce((prev, next) => prev + next.weight, 0).toFixed(2))
+
+const completedEntries = computed(() => entries.value.filter(entry => entry.done == true))
+const completedEntryCount = computed(() => completedEntries.value.length)
+const completedEntryWeight = computed(() => completedEntries.value.reduce((prev, next) => prev + next.weight, 0).toFixed(2))
+const completedPercentage = computed(() => ((((completedEntryWeight.value / entryWeight.value)) * 100).toFixed()))
+
+export default {
+  state,
+  entries,
+  entryCount,
+  entryWeight,
+  completedEntries,
+  completedEntryCount,
+  completedEntryWeight,
+  completedPercentage
+}
